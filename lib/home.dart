cff831b0ca5983ef4b402eb1528eccd794627762
Nipuni Premadasa <nipunipremadasa923@gmail.com>
2022-07-02 13:53:33 +0530

    add tabs

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,13 +8,38 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  TabController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(initialIndex: 0, length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text("Markdown Editor"),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        title: const Text("Markdown Editor"),
+        bottom: TabBar(
+          controller: _controller,
+          tabs: [
+            Tab(
+              text: "Editor",
+            ),
+            Tab(
+              text: "Previw",
+            ),
+          ],
+          indicatorColor: Colors.white,
+        ),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: [Icon(Icons.add), Icon(Icons.poll)],
       ),
     );
   }
