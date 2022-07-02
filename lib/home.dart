a912a9331a495491ac4f7da7d241a71ebb372b61
Nipuni Premadasa <nipunipremadasa923@gmail.com>
2022-07-02 14:52:19 +0530

    add markdown body

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,8 @@ import 'dart:ffi';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:simple_markdown_editor/simple_markdown_editor.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,11 +12,15 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   TabController? _controller;
+  TextEditingController? _textController;
+
+  String text = "";
 
   @override
   void initState() {
     super.initState();
     _controller = TabController(initialIndex: 0, length: 2, vsync: this);
+    _textController = TextEditingController();
   }
 
   @override
@@ -26,7 +32,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
         title: const Text("Markdown Editor"),
         bottom: TabBar(
           controller: _controller,
-          tabs: [
+          tabs: const [
             Tab(
               text: "Editor",
             ),
@@ -39,8 +45,55 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
       ),
       body: TabBarView(
         controller: _controller,
-        children: [Icon(Icons.add), Icon(Icons.poll)],
+        children: [
+          Container(
+            child: MarkdownFormField(
+              controller: _textController,
+              enableToolBar: true,
+              emojiConvert: true,
+              autoCloseAfterSelectEmoji: false,
+              onChanged: (String text) {
+                setState(() {
+                  this.text = text;
+                });
+              },
+            ),
+          ),
+          Container(
+            child: MarkdownBody(data: text),
+          )
+
+          //Preview(data: _textController.text),
+        ],
       ),
     );
   }
 }
+
+// class Preview extends StatelessWidget {
+//   const Preview({Key? key, required this.data}) : super(key: key);
+
+//   final String data;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Markdown Parse"),
+//       ),
+//       body: MarkdownParse(
+//         data: data,
+//         onTapHastag: (String name, String match) {
+//           // example : #hashtag
+//           // name => hashtag
+//           // match => #hashtag
+//         },
+//         onTapMention: (String name, String match) {
+//           // example : @mention
+//           // name => mention
+//           // match => #mention
+//         },
+//       ),
+//     );
+//   }
+// }
